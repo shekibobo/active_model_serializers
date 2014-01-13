00f9b68d66928588b3f65bfcbfeb895a0d1c858d
Adrian Mugnolo and Santiago Pastorino <adrian+santiago@wyeworks.com>
2014-01-13 17:43:59 -0200

    WIP

--- lib/active_model/serializer/configuration.rb
+++ lib/active_model/serializer/configuration.rb
@@ -2,50 +2,45 @@ require 'singleton'
 
 module ActiveModel
   class Serializer
-    class Configuration
-      class Null
-        include Singleton
-
-        def method_missing(*)
-          nil
-        end
-
-        def respond_to?(*)
-          true
-        end
-      end
-
-      attr_accessor :parent
+    class GlobalConfiguration
+      include Singleton
 
       class << self
-        def global
-          @global ||= new default_options
-        end
-
         def default_options
           { embed: :objects, meta_key: :meta }
         end
       end
 
-      def build(options = {})
-        self.class.new options, self
+      attr_accessor :root, :embed, :embed_in_root
+
+      def initialize
+        self.class.default_options.each do |name, value|
+          send "#{name}=", value
+        end
       end
 
-      attr_reader :scope, :each_serializer, :resource_name
-      attr_writer :root, :meta, :meta_key
-      attr_accessor :wrap_in_array
+      def method_missing(*)
+        nil
+      end
 
-      def initialize(options = {}, parent = Null.instance)
-        @parent          = parent
-        @root            = read_option options, :root
-        @embed           = read_option options, :embed
-        @embed_in_root   = read_option options, :embed_in_root
-        @scope           = options[:scope]
-        @meta_key        = read_option options, :meta_key
-        @meta            = read_option options, meta_key
-        @wrap_in_array   = options[:_wrap_in_array]
-        @each_serializer = options[:each_serializer]
-        @resource_name   = options[:resource_name]
+      def respond_to?(*)
+        true
+      end
+    end
+
+    class ClassConfiguration
+      attr_accessor :parent
+      attr_writer :root, :embed, :embed_in_root
+
+      def initialize(parent = GlobalConfiguration.instance, options = {})
+        @parent        = parent
+        @root          = read_option options, :root
+        @embed         = read_option options, :embed
+        @embed_in_root = read_option options, :embed_in_root
+      end
+
+      def build(options = {})
+        self.class.new options, self
       end
 
       def root
@@ -58,14 +53,6 @@ module ActiveModel
 
       def embed_in_root
         return_first @embed_in_root, parent.embed_in_root
-      end
-
-      def meta_key
-        return_first @meta_key, parent.meta_key
-      end
-
-      def meta
-        return_first @meta, parent.meta
       end
 
       # FIXME: Get rid of this mess.
@@ -94,6 +81,38 @@ module ActiveModel
 
       def return_first(*values)
         values.compact.first
+      end
+    end
+
+    class InstanceConfiguration < ClassConfiguration
+      # FIXME attr_reader :scope needed
+      attr_writer :scope, :meta, :meta_key
+      attr_accessor :wrap_in_array
+
+      def initialize(parent, options)
+        super
+        @scope         = options[:scope]
+        @meta_key      = read_option options, :meta_key
+        @meta          = read_option options, meta_key
+        @wrap_in_array = options[:_wrap_in_array]
+      end
+
+      def meta_key
+        return_first @meta_key, parent.meta_key
+      end
+
+      def meta
+        return_first @meta, parent.meta
+      end
+    end
+
+    class ArrayConfiguration
+      attr_accessor :each_serializer, :resource_name
+
+      def initialize(parent = GlobalConfiguration.instance, options = {})
+        @parent = parent
+        @each_serializer = options[:each_serializer]
+        @resource_name   = options[:resource_name]
       end
     end
   end
